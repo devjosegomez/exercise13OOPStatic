dae8b2333dbe06337226565b988bfa4ea39864b5
devjosegomez <devjosegomez@gmail.com>
2019-10-18 11:57:34 -0500

    Getters and setters SerialNumbers, NumberDoors

--- src/com/exercise13oopStatic/model/Car.java
+++ src/com/exercise13oopStatic/model/Car.java
@@ -1,5 +1,36 @@
 package com.exercise13oopStatic.model;
 
 public class Car {
+	private String serialNumber;
+	private int numberDoors;
+	
+	public Car(){
+		
+	}
+	
+	public Car(String serialNumber, int numberDoors){
+		this.serialNumber = serialNumber;
+	}
+	
+	 
+	//Getters and setters serialNumber
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	
+	public void setSerialNumber() {
+		this.serialNumber = serialNumber;
+	}
+	
+	//Getters and setters NumbersDoors
+	public int getNumberDoors() {
+		return numberDoors;
+	}
+	
+	public void setNumberDoors() {
+		this.numberDoors = numberDoors;
+	}
+	
+	
 	
 }
